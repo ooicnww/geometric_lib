08e6e9e43c174f34af0661ce699b5afa313ff91d
Olga <gusinao652@gmail.com>
2024-10-01 19:01:05 +0300

    Добавлены изменения в файлы

--- calculate.py
+++ calculate.py
@@ -1,33 +1,31 @@
-import circle
-import square
 
-
-figs = ['circle', 'square']
-funcs = ['perimeter', 'area']
-sizes = {}
-
+import circle  # Импорт модуля circle
+import square  # Импорт модуля square
+figs = ['circle', 'square']  # Список доступных фигур
+funcs = ['perimeter', 'area']  # Список доступных функций
+sizes = {}  # Словарь для хранения размеров фигур
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
-
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
-
-if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
-
-
-
+    """
+    Вычисляет периметр или площадь заданной фигуры.
+    Параметры:
+    - fig (str): название фигуры ('circle' или 'square')
+    - func (str): название функции ('perimeter' или 'area')
+    - size (list): список размеров фигуры
+    Возвращает:
+    - None
+    """
+    assert fig in figs  # Проверка, что фигура находится в списке доступных фигур
+    assert func in funcs  # Проверка, что функция находится в списке доступных функций
+    result = eval(f'{fig}.{func}(*{size})')  # Вычисление периметра или площади фигуры
+    print(f'{func} of {fig} is {result}')  # Вывод результата
+if name == "__main__":
+    func = ''  # Инициализация переменной для хранения названия функции
+    fig = ''  # Инициализация переменной для хранения названия фигуры
+    size = list()  # Инициализация списка для хранения размеров фигуры
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}:\n")  # Запрос названия фигуры у пользователя
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}:\n")  # Запрос названия функции у пользователя
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))  # Запрос размеров фигуры у пользователя
+    calc(fig, func, size)  # Вызов функции calc с введенными параметрами
